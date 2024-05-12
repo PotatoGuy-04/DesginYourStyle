4feec5f225f119c0793d9cbfd17a127746f900de
Yazan Aldarra <147935647+PotatoGuy-04@users.noreply.github.com>
2024-05-12 12:38:43 +0200

    Delete js directory

--- js/new.js
+++
@@ -1,9 +0,0 @@
-/* hamburger menu */
-const hamburgerMenuCheckbox = document.querySelector('.hamburger-menu input[type="checkbox"]');
-const bootstrap2Links = document.querySelectorAll('.bootstrap2 a');
-
-bootstrap2Links.forEach(link => {
-  link.addEventListener('click', () => {
-    hamburgerMenuCheckbox.checked = false;
-  });
-});
